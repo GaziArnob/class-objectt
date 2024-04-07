f4810884483971fa7ec1aad651a0d03df2ddab33
GaziArnob <arnob2224@gmail.com>
2024-04-07 16:26:51 +0600

    secend commit

---
+++ class&object.js
@@ -0,0 +1,50 @@
+/*const student={
+    name:"gazi arnob",
+    marks: 94.4,
+    printMarks: function() {
+        console.log("marks=",this. marks);
+    },
+};  
+
+const employee = {
+    calcTax() {
+        console.log("tax rate is 10%");
+    },
+
+}
+
+const GaziArnob ={
+salary:50000,
+calcTax() {
+    console.log("tax rate is 20%");
+}
+};
+const GaziArnob1 ={
+    salary:50000,
+    };
+    const GaziArnob2 ={
+        salary:50000,
+        };
+        const GaziArnob3 ={
+            salary:50000,
+            };
+            const GaziArnob4 ={
+                salary:50000,
+                };
+GaziArnob.__proto__ = employee;
+GaziArnob1.__proto__ = employee;
+GaziArnob2.__proto__ = employee;
+GaziArnob3.__proto__ = employee;
+GaziArnob4.__proto__ = employee;*/
+
+class Toyotacar{
+start( ) {
+   console.log("start") ;
+}
+stop() {
+    console.log("stop");
+}
+}
+
+let fortuner = new ToyotaCar();
+let lexus = new ToyotaCar();
